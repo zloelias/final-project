b44a8d53d4e1c4729e56e323a2f04b2969088bad
zloelias <zloelias@gmail.com>
2021-12-01 00:02:43 +0300

    prepare datasets

---
+++ datasets/kinopoisk.py
@@ -0,0 +1,23 @@
+import glob
+from datasets import concatenate_datasets, load_dataset
+
+neg = glob.glob("./data/kinopoisk/dataset/neg/*")
+neu = glob.glob("./data/kinopoisk/dataset/neu/*")
+pos = glob.glob("./data/kinopoisk/dataset/neu/*")
+
+kinopoisk_dataset1 = load_dataset('text', data_files=neg)
+kinopoisk_dataset1 = kinopoisk_dataset1.map(lambda x: {'text': x['text'], 'labels': -1, 'label_name': 'negative'})
+
+kinopoisk_dataset2 = load_dataset('text', data_files=neu)
+kinopoisk_dataset2 = kinopoisk_dataset2.map(lambda x: {'text': x['text'], 'labels': 0, 'label_name': 'neutral'})
+
+kinopoisk_dataset3 = load_dataset('text', data_files=pos)
+kinopoisk_dataset3 = kinopoisk_dataset2.map(lambda x: {'text': x['text'], 'labels': 1, 'label_name': 'positive'})
+
+kinopoisk_dataset = concatenate_datasets([kinopoisk_dataset1['train'], kinopoisk_dataset2['train'], kinopoisk_dataset3['train']])
+
+kinopoisk_dataset = kinopoisk_dataset.train_test_split(test_size=0.1, train_size=0.9)
+
+
+kinopoisk_dataset.push_to_hub(repo_id='zloelias/kinopoisk-reviews')
+
---
+++ datasets/lenta_ru.py
@@ -0,0 +1,10 @@
+import datasets
+from datasets import load_dataset
+
+lenta_dataset = load_dataset('csv', data_files='./data/lenta_ru/lenta-ru-train.csv')['train']
+lenta_dataset = lenta_dataset.train_test_split(test_size=0.1, train_size=0.9)
+
+lenta_dataset['train'].rename_column_('topic_label', 'labels')
+lenta_dataset['test'].rename_column_('topic_label', 'labels')
+
+lenta_dataset.push_to_hub(repo_id='zloelias/lenta-ru')
