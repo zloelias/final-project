1e49ca74b30a4a1f56d5f18a53ce8ff0cf02df8f
zloelias <zloelias@gmail.com>
2021-12-06 23:09:12 +0300

    fix kinopoisk data: balance classes

--- datasets/kinopoisk.py
+++ datasets/kinopoisk.py
@@ -13,16 +13,6 @@ def clearRating(s):
         s = s.replace(str(i)+' из 10', '')
     return s
 
-i = 0
-for file in neg:
-    with open(file, 'r') as f:
-        text = clearRating(f.read())
-        negative.append({'text': text, 'labels': 2, 'label_name': 'negative'})
-        i += 1
-        #print('--------')
-        #print(data[-1])
-print(f'negative :{i}')
-
 neutral = []
 i = 0
 for file in neu:
@@ -37,21 +27,31 @@ positive = []
 i = 0
 for file in pos:
     with open(file, 'r') as f:
-        positive.append({'text': clearRating(f.read()), 'labels': 1, 'label_name': 'positive'})
+        positive.append({'text': clearRating(f.read()), 'labels': 0, 'label_name': 'positive'})
         i += 1
         #print('--------')
         #print(data[-1])
 print(f'positive :{i}')
 
+i = 0
+for file in neg:
+    with open(file, 'r') as f:
+        text = clearRating(f.read())
+        negative.append({'text': text, 'labels': 1, 'label_name': 'negative'})
+        i += 1
+        #print('--------')
+        #print(data[-1])
+print(f'negative :{i}')
+
 import pandas as pd
 
-negative = pd.DataFrame(negative).sample(len(negative)*2, replace=True)
+negative = pd.DataFrame(negative).sample(frac=1.5, replace=True)
 negative.dropna()
 
-neutral = pd.DataFrame(neutral).sample(len(neutral)*2, replace=True)
+neutral = pd.DataFrame(neutral).sample(frac=0, replace=True)
 neutral.dropna()
 
-positive = pd.DataFrame(positive).sample(len(positive)//2, replace=True)
+positive = pd.DataFrame(positive).sample(frac=0.6, replace=True)
 positive.dropna()
 
 print('--------------------------------')
@@ -60,7 +60,8 @@ print(f'negative: {negative.shape[0]}')
 print(f'neutral: {neutral.shape[0]}')
 print(f'positive: {positive.shape[0]}')
 
-data = [negative, neutral, positive]
+#data = [negative, neutral, positive]
+data = [negative, positive]
 df = pd.concat(data)
 
 
