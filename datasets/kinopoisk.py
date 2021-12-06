5af7d1991e83dbe7fc6f9363bdab408bbdf7eabf
zloelias <zloelias@gmail.com>
2021-12-06 17:56:27 +0300

    fix kinopoisk data: balance classes

--- datasets/kinopoisk.py
+++ datasets/kinopoisk.py
@@ -5,7 +5,7 @@ neg = glob.glob("./data/kinopoisk/dataset/neg/*")
 neu = glob.glob("./data/kinopoisk/dataset/neu/*")
 pos = glob.glob("./data/kinopoisk/dataset/pos/*")
 
-data = []
+negative = []
 
 
 def clearRating(s):
@@ -13,28 +13,56 @@ def clearRating(s):
         s = s.replace(str(i)+' из 10', '')
     return s
 
+i = 0
 for file in neg:
     with open(file, 'r') as f:
-        data.append({'text': clearRating(f.read()), 'labels': 2, 'label_name': 'negative'})
+        text = clearRating(f.read())
+        negative.append({'text': text, 'labels': 2, 'label_name': 'negative'})
+        i += 1
         #print('--------')
         #print(data[-1])
+print(f'negative :{i}')
 
+neutral = []
+i = 0
 for file in neu:
     with open(file, 'r') as f:
-        data.append({'text': clearRating(f.read()), 'labels': 0, 'label_name': 'neutral'})
+        neutral.append({'text': clearRating(f.read()), 'labels': 0, 'label_name': 'neutral'})
+        i += 1
         #print('--------')
         #print(data[-1])
+print(f'neutral :{i}')
 
+positive = []
+i = 0
 for file in pos:
     with open(file, 'r') as f:
-        data.append({'text': clearRating(f.read()), 'labels': 1, 'label_name': 'positive'})
+        positive.append({'text': clearRating(f.read()), 'labels': 1, 'label_name': 'positive'})
+        i += 1
         #print('--------')
         #print(data[-1])
+print(f'positive :{i}')
 
 import pandas as pd
 
-df = pd.DataFrame(data).sample(frac=1)
-df.dropna()
+negative = pd.DataFrame(negative).sample(len(negative)*2, replace=True)
+negative.dropna()
+
+neutral = pd.DataFrame(neutral).sample(len(neutral)*2, replace=True)
+neutral.dropna()
+
+positive = pd.DataFrame(positive).sample(len(positive)//2, replace=True)
+positive.dropna()
+
+print('--------------------------------')
+print('After resampling:')
+print(f'negative: {negative.shape[0]}')
+print(f'neutral: {neutral.shape[0]}')
+print(f'positive: {positive.shape[0]}')
+
+data = [negative, neutral, positive]
+df = pd.concat(data)
+
 
 kinopoisk_dataset = Dataset.from_pandas(df)
 kinopoisk_dataset = kinopoisk_dataset.train_test_split(test_size=0.1, train_size=0.9)
